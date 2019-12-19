dadede89e50b13df0631b4b6fb9881226cfb8032
Miguel Arbesú <miguelarbesu@gmail.com>
2019-12-19 10:21:29 +0100

    Improves Markdown style
    Dashded lines were interpreted as emphasis by some MD renderers (Okular...)

--- get_exptitles.sh
+++ get_exptitles.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 # Generates a experiment list (explist.md) for all
 # Topspin experiments in the current folder. 
@@ -6,21 +6,24 @@
 # title file are written down. 
 
 expList=./explist.md
+
 currentPath=`pwd`
 expFolder=(`basename $currentPath`)
 longDash=(`printf '%.0s-' {1..79}`)
-shortDash=(`printf '%.0s-' {1..15}`)
 
 echo $expFolder > $expList
+printf '\n' >> $expList
 echo $longDash >> $expList
+printf '\n' >> $expList
 
 # Enable + wildcard
 shopt -s extglob
 for exp in $(ls -vd ./+([0-9]))
 do
     echo $exp >> $expList
-    echo $shortDash >> $expList
+    printf '\n' >> $expList
     cat $exp/pdata/1/title >> $expList
     printf '\n' >> $expList
-    echo $longDash >> $expList    
+    echo $longDash >> $expList
+    printf '\n' >> $expList
 done
